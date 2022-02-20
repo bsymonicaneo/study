1ca326eb45e7e6aeeca95013bb7f9eac939a5295
bsymonica <bsymoncia@neowiz.com>
2022-02-20 18:38:15 +0900

    1 수정!

--- src/ex-1.js
+++ src/ex-1.js
@@ -1,3 +1,5 @@
 // 사용자의 이름을 입력 받아서 다음과 같이 출력하라. Hello [이름]
-let firstname = 'Sunyoung';
+import { question } from "readline-sync";
+
+const firstname = question('이름을 입력해주세요.');
 console.log('Hello' + ' ' + firstname);
