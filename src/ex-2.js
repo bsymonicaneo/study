18d5cbde2fca85c0bee7bd9c31b2c82d64894c8d
bsymonica <bsymoncia@neowiz.com>
2022-02-20 18:40:04 +0900

    2 수정!

--- src/ex-2.js
+++ src/ex-2.js
@@ -1,6 +1,7 @@
 // 사용자의 이름을 입력받은 다음, 
 // 사용자의 성을 입력받아서 다음과 같이 출력하라. Hello [이름] [성]
-'use strict';
-let firstname = 'Sunyoung';
-let lastname = 'Baek';
+import { question } from "readline-sync";
+
+const firstname = question('이름을 입력해주세요.');
+const lastname = question('성을 입력해주세요.');
 console.log('Hello' + ' ' + firstname + ' ' + lastname);
