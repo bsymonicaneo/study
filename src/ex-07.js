a6a1fde7de1fa88653841a099578998d388f844f
bsymonica <bsymoncia@neowiz.com>
2022-02-20 19:06:10 +0900

    07 해결!

--- src/ex-07.js
+++ src/ex-07.js
@@ -1,4 +1,9 @@
-let age = 38;
-let name = 'bsymonica';
-console.log(name + ' ' + 'next birthday you will be' + ' ' + (age + 1));
-// 숫자는 '' 안에 넣어서 문자가 되지 않도록 주의 
+// 사용자로부터 이름과 나이를 입력 받아서 나이에 1을 더한 후 다음과 같이 출력하라.
+
+import { question } from "readline-sync";
+
+const name = question('이름을 입력 해주세요.');
+const age = question('나이를 입력 해주세요.');
+console.log(name + 'next birthday you will be ' + (Number (age) + 1));
+
+// 숫자는 '' 안에 넣어서 문자가 되지 않도록 주의
