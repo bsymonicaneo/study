0b26a395bcaa69fce6e7cb110d3c061c574a0d57
bsymonica <bsymoncia@neowiz.com>
2022-02-20 18:47:20 +0900

    6 해결!

--- src/ex-6.js
+++ src/ex-6.js
@@ -1,6 +1,9 @@
-let a = 8;
-let b = 2;
-console.log('남은 피자 조각 : ' + ' ' + (a - b));
 // 사용자로부터 처음에 가지고 있었던 피자 조각 수를 입력받고, 
 // 몇 조각을 먹었는지 입력받아서 남은 조각 수를 계산하여 
 // 사람에게 익숙한 형식으로 출력하라.
+
+import { question } from "readline-sync";
+
+const pizza = question('처음에 가지고 있었던 피자 조각 수를 입력 하세요.');
+const eat = question('몇 조각 먹었는지 입력 하세요.');
+console.log('남은 피자 조각 :', Number(pizza) - Number(eat));
