e0b2ac5ab7fdc3c255a414e5f608e3c0c8aec258
bsymonica <bsymoncia@neowiz.com>
2022-02-21 00:43:54 +0900

    10 해결!

--- src/ex-10.js
+++ src/ex-10.js
@@ -1,4 +1,7 @@
 // 1킬로그램은 2.204 파운드다.
 // 몸무게를 킬로그램 단위로 입력받아서 파운드로 변환하여 출력하라
-let pound = (1 * 2.204);
-console.log(`몸무게는: ${pound} 파운드 입니다.`);
+import { question } from "readline-sync";
+
+const kg = question('몸무게kg을 입력 하세요.');
+const pound = ( kg * 2.204);
+console.log(`몸무게는 ${pound} 파운드 입니다.`);
