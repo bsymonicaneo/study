8ec6a3c80df6e949bfcbcf861a0a3c7806a66e6f
bsymonica <bsymoncia@neowiz.com>
2022-03-02 15:24:38 +0900

    22 해결!

--- src/ex-22.js
+++ src/ex-22.js
@@ -7,3 +7,16 @@ const name2 = question('성을 소문자로 입력하세요');
 const string_1 = name1.slice(0,1).toUpperCase() + name1.slice(1);
 const string_2 = name2.slice(0,1).toUpperCase() + name2.slice(1);
 console.log(`${string_1} ${string_2}`);
+
+/* 
+Const fname1 = question('이름을 소문자로 입력하세요');
+const fname2 = question('성을 소문자로 입력하세요');
+const fname11 = fname1.slice(0, 1);
+const fname22 = fname2.slice(0, 1);
+const fname =
+    fname11.toUpperCase() + 
+    fname1.slice(1) +
+    ' ' +
+    fname22.toUpperCase() +
+    fname2.slice(1);
+conselo.log('이름 성:' + fname); */
