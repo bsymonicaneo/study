16032d495aab46dafd10c5f87e47be45d584bd38
bsymonica <bsymoncia@neowiz.com>
2022-04-03 19:58:19 +0900

    틀린거 수정!

--- src/ex-50.js
+++ src/ex-50.js
@@ -6,22 +6,16 @@
 
 import { question } from 'readline-sync';
 
-const usernum = question('10과 20 사이의 숫자를 입력하라 : ');
-let i = usernum; //초기값을 밖에서 세팅
+let userNum = 0; //초기값 바깥에서 세팅
 
-if (usernum <= 10) {
-  console.log('Too low');
-  //const usernum = question('10과 20 사이의 숫자를 입력하라 : ');
-} else if (usernum > 10 && usernum <= 20) {
-  // 10 과 20 사이의 숫자
+while (userNum <= 10 || userNum >= 20) {
+  userNum = question('10과 20 사이의 숫자를 입력하세요 : ');
 
-  console.log('Thank you');
-  if (usernum > 20) {
-    console.log('Too high');
-    const usernum = question('10과 20 사이의 숫자를 입력하라 : ');
-    while (i <= 20) {
-      console.log('i');
-      i++;
-    }
+  if (userNum <= 10) {
+    console.log('Too low');
+  } else if (userNum >= 20) {
+    console.log('Too High');
+  } else {
+    console.log('Thank you');
   }
 }
